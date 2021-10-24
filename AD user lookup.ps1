2ec1510007ea4239392919d57318c916ef40b2cc
tddimitrov <tddimitrov@uams.edu>
2021-10-24 12:55:19 -0500

    adding comments

--- AD user lookup.ps1
+++ AD user lookup.ps1
@@ -1,10 +1,15 @@
+#Use "Run Selection" to pick which code to execute.
+
+#"Traditional" search by part of displayname
+# output will be more verbose  and you can see more AD attributes
 $displayname = Read-Host "enter name"
 $namelookup = "*$displayname*"
 Get-ADUser -Filter {name -like $namelookup} -properties * | Select-Object displayname, samaccountname, UserPrincipalName, employeenumber, lockedout, enabled, PasswordLastSet, LastBadPasswordAttempt, title | sort displayname |
 Format-Table -AutoSize 
 
 
-#ambiguous name resolution
+#Search based on ANR - ambiguous name resolution
+#Quicker and more efficient searh but returns fewer attributes than the first one
 $partofname = Read-Host "enter name"
 Get-ADUser -LDAPFilter "(anr=$partofname)" | select name, samaccountname, userprincipalname | sort Name
 
